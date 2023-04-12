c3d50d3d379a7c43ed585cd64bdcfffd16a69682
Daniel Ziegler <daniel.ziegler@senacor.com>
2023-04-12 13:37:11 +0200

    Add vulnerable dependency

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.google.common.io.Files;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,11 @@ public class DemoController {
 
     @GetMapping(path = "/api/test")
     public ResponseEntity<String> getTest() {
+        // The following api call would be insecure
+        // var tmpDir = Files.createTempDir();
+
+        String extension = Files.getFileExtension("foo.txt");
+
         return ResponseEntity.ok("Test successful");
     }
 }
